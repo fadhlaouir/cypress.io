4109962471bd9aa86bbed4e450de344f34980354
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-05-07 13:33:50 +0100

    working with command results
    working with command results

---
+++ cypress/integration/results.spec.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+
+
+describe('Text box with max characters', () => {
+    it('displays the appropriate remaining characters count', () => {
+        cy.visit('http://localhost:3000/example-3');
+
+        cy.get('[data-cy="last-name-chars-left-count"]')
+        .as('charsLeftSpan');
+
+        cy.get('[data-cy="input-last-name"]')
+        .as('charInput')
+
+        cy.get('@charsLeftSpan').then($charsLeftSpan => {
+            expect($charsLeftSpan.text()).to.equal('15')
+        })
+ 
+        cy.get('@charInput')
+            .type('hello');
+
+        cy.get('@charsLeftSpan')
+            .invoke('text')
+            .should('equal', '10');
+
+        cy.get('@charInput')
+            .type(' my firend');
+
+        cy.get('@charsLeftSpan')
+            .invoke('text')
+            .should('equal', '0');
+    });
+
+    it('prevent the user from typing more characters once max is exceeded', () => {
+        cy.visit('http://localhost:3000/example-3');
+
+        cy.get('[data-cy="input-last-name"]')
+        .as('charInput')
+
+        cy.get('@charInput').type('qwertyuiopasdfghjkl;xcvbn');
+        cy.get('@charInput').should('have.attr', 'value', 'qwertyuiopasdfg');
+    })
+})
